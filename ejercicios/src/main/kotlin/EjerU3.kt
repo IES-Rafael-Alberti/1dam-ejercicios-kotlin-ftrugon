61aa196191375fbcdabd4a811d8be91b05a05eb5
franconil <franconil04@gmail.com>
2024-01-03 22:47:30 +0100

    joder con los 2 ultimos

--- ejercicios/src/main/kotlin/EjerU3.kt
+++ ejercicios/src/main/kotlin/EjerU3.kt
@@ -128,11 +128,264 @@ fun listuplu3ej13() {
     for (i in matriz1.indices){
         val matrizcolumna = mutableListOf<Int>()
 
-        for (x in 0 until   matriz1[0].size) {
+        for (x in 0 until matriz1[0].size) {
 
             matrizcolumna.add(matriz1[i][x] * matriz2[i][x])
         }
         matrizfila.add(matrizcolumna)
     }
     println("$matriz1 * $matriz2 = $matrizfila")
+}
+// ------------------------------------------------------------------------------------------------------------------
+/**
+ * Guarda en un diccionario los precios de las frutas de la tabla, pregunta al usuario por una
+ * fruta, un número de kilos y muestre por pantalla el precio de ese número de kilos de fruta. Si la fruta no está en
+ * el diccionario muestra un mensaje informando de ello.
+ */
+fun dicciou3ej3(){
+    try {
+        val precios = mapOf(
+            "platano" to 1.35,
+            "manzana" to 0.80,
+            "pera" to 0.85,
+            "naranja" to 0.70
+        )
+
+        println("Platano -> 1.35")
+        println("Manzana -> 0.80")
+        println("Pera -> 0.85")
+        println("Naranja -> 0.70")
+        println("Todo es por Kg")
+
+        print("Dime que fruta quieres: ")
+
+        val elegir = readln().lowercase().trim()
+        if (elegir in precios){
+            print("Cuantos kilos quieres?: ")
+            val cuantas = readln().toFloat()
+            println("$cuantas Kg de $elegir -> ${String.format("%.2f",cuantas * precios[elegir]!!)}" )
+        }else{
+            println("La fruta no esta en el catalogo")
+        }
+    }catch (e:Exception){
+        println("eror inesperado")
+    }
+}
+// ------------------------------------------------------------------------------------------------------------------
+/**
+ * Tiene un diccionario con asignaturas y los creditos de cada asignatura, por cada asignatura
+ * muestra sus creditos
+ */
+fun dicciou3ej5(){
+    val diccio = mapOf(
+        "matematicas" to 6,
+        "Física" to 4,
+        "Química" to 5
+    )
+    for ((asignatura, creditos) in diccio){
+        println("En $asignatura tienes $creditos creditos")
+    }
+}
+// ------------------------------------------------------------------------------------------------------------------
+/**
+ * Es un diccionario que se va rellenando con informacion que eliga el usuario
+ */
+fun dicciou3ej6(){
+    try {
+        val diccionariodepersona = mutableMapOf<String,String>()
+        var estado = false
+        while (!estado){
+            print("Que llave quieres añadir(no pongas nada si quieres acabar): ")
+            val cosaanadir = readln()
+            if (cosaanadir == ""){
+                estado = true
+            }else{
+                print("Dime el valor de $cosaanadir: ")
+                val valor = readln()
+                diccionariodepersona[cosaanadir] = valor
+                println()
+                for ((anadir, valo) in diccionariodepersona){
+                    println("La clave |$anadir| tiene como valor |$valo|")
+                }
+            }
+        }
+    }catch (e:Exception){
+        println("Error inesperado")
+    }
+}
+// ------------------------------------------------------------------------------------------------------------------
+/**
+ * Es una lista de la compra, le vas ntroduciendo el articulo y su precio y al final te da la lista y el total
+ */
+fun dicciou3ej7(){
+
+    val listacompra = mutableMapOf<String,Float>()
+    var estado = false
+    var total = 0.0
+    while (!estado){
+        print("Que quieres añadir a la lista de la compra(no pongas nada si quieres acabar): ")
+        val cosaanadir = readln()
+        if (cosaanadir == ""){
+            estado = true
+        }else{
+            print("Dime el precio de $cosaanadir: ")
+            val valor = try {
+                readln().toFloat()
+            }catch (e:Exception){
+                println("error, el coste de $cosaanadir sera 0")
+                0.toFloat()
+            }
+            total += valor
+            listacompra[cosaanadir] = valor
+            println()
+        }
+    }
+        for ((anadir, valo) in listacompra){
+            println("$anadir -> $valo")
+        }
+    println("Por un total de $total")
+}
+// ------------------------------------------------------------------------------------------------------------------
+/**
+ * Crea un diccionario con las traducciones y la frase que tu metas cambia cada palabra por la que esta en el diccionario
+ * si no esta en el diccionario es la palabra sin traducir la que se queda
+ */
+fun dicciou3ej8() {
+    try {
+        val diccio = mutableMapOf<String, String>()
+        print("Dime la traduccion: ")
+        val intro = readln()
+        val separa = intro.split(",")
+        for (trad in separa) {
+            val massepara = trad.split(":")
+            if (massepara.size == 2){
+                diccio[massepara[0].trim()] = massepara[1].trim()
+            }
+        }
+        println("Dime una frase con las palabras a traducir: ")
+        val frase = readln()
+        val listafrase = frase.split(" ")
+        for (palabra in listafrase){
+            val traduccion = diccio[palabra.trim()] ?: palabra.trim()
+            print("$traduccion ")
+        }
+        println()
+    }catch (e:Exception){
+        println("Error inesperado")
+    }
+}
+// ------------------------------------------------------------------------------------------------------------------
+/**
+ * Es una agenda con clietnes
+ */
+fun dicciou3ej10(){
+    val clientes = mutableMapOf<String,MutableMap<String,Any>>()
+    var opcion: Int
+    do {
+        println("Menú:")
+        println("1. Añadir cliente")
+        println("2. Eliminar cliente")
+        println("3. Mostrar cliente")
+        println("4. Listar todos los clientes")
+        println("5. Listar clientes preferentes")
+        println("6. Terminar")
+        print("Seleccione una opción: ")
+        opcion = readln().toInt()?: 0
+        when (opcion){
+            1 -> {
+                val cliente = mutableMapOf<String, Any>()
+                print("Introduzca el NIF del cliente: ")
+                val nif = readln()
+                print("Nombre del cliente: ")
+                val nombre = readln()
+                print("Dirección del cliente: ")
+                val direccion = readln()
+                print("Teléfono del cliente: ")
+                val telefono = readln()
+                print("Correo del cliente: ")
+                val correo = readln()
+                print("¿Es cliente preferente? (s/n): ")
+                val preferente = readln()
+                var siono = false
+                if (preferente == "s"){
+                    siono = true
+                }
+
+                cliente["Nombre"] = nombre
+                cliente["Dirección"] = direccion
+                cliente["Teléfono"] = telefono
+                cliente["Correo"] = correo
+                cliente["Preferente"] = siono
+
+                clientes[nif] = cliente
+            }
+            2 -> {
+                print("Introduzca el NIF del cliente a eliminar: ")
+                val nifEliminar = readln()
+                if (nifEliminar in clientes.keys){
+                    clientes.remove(nifEliminar)
+                    println("se ha eliminado el cliente")
+                }else{
+                    println("El cliente no esta en la base de datos")
+                }
+            }
+            3 -> {
+                print("Introduzca el NIF del cliente a mostrar: ")
+                val nifMostrar = readln()
+                val clienteMostrado = clientes[nifMostrar]
+                if (clienteMostrado != null) {
+                    println("Datos del cliente con NIF $nifMostrar: $clienteMostrado")
+                } else {
+                    println("No se encontró ningún cliente con ese NIF.")
+                }
+            }
+            4 -> {
+                println("Mostrando todos los clientes: ")
+                for ((cli,valor) in clientes){
+                    println("$cli: $valor")
+                }
+            }
+            5 -> {
+                println("Lista de clientes preferentes:")
+                for ((cli,valor) in clientes){
+                    if (valor["Preferente"] == true){
+                        println("$cli: $valor")
+                    }
+                }
+            }
+            6 -> println("saliendo del programa")
+            else -> println("Opcion no valida")
+        }
+    }while (opcion != 6)
+}
+// ------------------------------------------------------------------------------------------------------------------
+/**
+ * Crea un diccionario de contactos a partir de un texto con saltos de linea
+ */
+fun dicciou3ej11(){
+    val texto = "nif;nombre;email;teléfono;descuento\n01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"
+
+    val div = texto.split("\n")
+
+    val campos = div[0].split(";")
+
+    val clientes = mutableMapOf<String,MutableMap<String,Any>>()
+
+    for (linea in div.subList(1,div.size)){
+
+        if (linea.isNotEmpty()) {
+
+            val valores = linea.split(";")
+            val infocli = mutableMapOf<String,Any>()
+
+            if (campos.size == valores.size){
+                for (i in 1 until valores.size){
+                    infocli[campos[i]] = valores [i]
+                }
+            }
+            clientes[valores[0]] = infocli
+        }
+    }
+    println(clientes)
+
 }
--- ejercicios/src/main/kotlin/Main.kt
+++ ejercicios/src/main/kotlin/Main.kt
@@ -24,6 +24,7 @@ fun main() {
     //condiu2ej6()
     //condiu2ej8()
     //condiu2ej10()
+
     //iteru2ej2()
     //iteru2ej4()
     //iteru2ej6()
@@ -34,6 +35,7 @@ fun main() {
     //iteru2ej18()
     //iteru2ej19()
     //iteru2ej25()
+
     //exepu2ej2()
     //exepu2ej3()
     //exepu2ej4()
@@ -45,4 +47,12 @@ fun main() {
     //listuplu3ej9()
     //listuplu3ej10()
     //listuplu3ej13()
+
+    //dicciou3ej3()
+    //dicciou3ej5()
+    //dicciou3ej6()
+    //dicciou3ej7()
+    //dicciou3ej8()
+    //dicciou3ej10()
+    dicciou3ej11()
 }
